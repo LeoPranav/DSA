bbc37500048ffcae7d0db647c23904f523305dc1
Pranavkumar <walkerpranav@gmail.com>
2025-10-18 22:22:01 +0530

    Selection Sort

--- Sorting/selectionSort.py
+++ Sorting/selectionSort.py
@@ -1,1 +1,11 @@
-print("hello world!")
+arr = [1,6,3,2,4]
+n = len(arr)
+
+for i in range(n):
+    mi = i
+    for j in range(i+1,n):
+        if arr[j]<arr[mi]:
+            mi=j
+    arr[mi],arr[i] = arr[i],arr[mi]
+
+print(arr)
---
+++ update.bat
@@ -0,0 +1,6 @@
+@echo off
+git add .
+git commit -m "%~1"
+git pull origin main --rebase
+git push origin main
+pause
