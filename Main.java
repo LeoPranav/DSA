1da60871cc7b8730f08c6ebba2795599495d948d
Pranavkumar <walkerpranav@gmail.com>
2025-10-18 22:09:45 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        System.out.println("You Entered :"+a);
+        sc.close();
+    }   
+}
